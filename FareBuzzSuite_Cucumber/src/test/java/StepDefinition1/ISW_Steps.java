e9bb085cf7439178910b38dbd0083e1f3df42b36
test <test@test-PC>
2020-08-10 15:04:47 +0530

    commit 2 -added to project

--- FareBuzzSuite_Cucumber/src/test/java/StepDefinition1/ISW_Steps.java
+++ FareBuzzSuite_Cucumber/src/test/java/StepDefinition1/ISW_Steps.java
@@ -1,20 +1,16 @@
 package StepDefinition1;
 
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.FileUtils;
+//import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
